48660e46953c5995c322404d86f18f089fe6e974
mmichaelnorward777 <67016919+mmichaelnorward777@users.noreply.github.com>
2021-09-17 01:40:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -131,8 +131,8 @@ mongoose.connect(process.env.PROD_DB_CONNECT, {
 
 
  // /* Views Routes... */
- app.get("*", (req, res) => {
-     res.sendFile(path.join(__dirname, "views", "index.html"));
- });
+//  app.get("*", (req, res) => {
+//      res.sendFile(path.join(__dirname, "views", "index.html"));
+//  });
 
 

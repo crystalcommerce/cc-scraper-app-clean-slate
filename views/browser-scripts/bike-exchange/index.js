c756de1cfe4d83a4a91684c9f025db042baf2f62
mmichaelnorward777 <67016919+mmichaelnorward777@users.noreply.github.com>
2022-10-13 06:40:40 +0800

    finished script for bike-exchange

--- views/browser-scripts/bike-exchange/index.js
+++ views/browser-scripts/bike-exchange/index.js
@@ -37,6 +37,7 @@ async function awaitGlobal({condition}) {
         CcBrowserWindow,
         CcScraperChannel,
         categorizedSetsOfflineDb,
+        productsOfflineDb,
     } = window.___cc__CcScraperGlobalObject,
     { decodedProcessFromUrl, currentProcess, initializeScraping } = urlProcessInitializer();
 
@@ -118,7 +119,7 @@ async function awaitGlobal({condition}) {
                                         subcategories.forEach(sub => {
                                             if(sub.value !== category.value)    {
                                                 categorizedSets.push({
-                                                    url : `https://www.bikeexchange.com/?cc-unique-url=${generateUuid()}`,
+                                                    url : `https://www.bikeexchange.com/?cc-unique-url=${sub.value}`,
                                                     label : sub.value,
                                                     setData : {
                                                         category : categoryName,
@@ -130,7 +131,7 @@ async function awaitGlobal({condition}) {
                                         })
                                     } else  {
                                         categorizedSets.push({
-                                            url : `https://www.bikeexchange.com/?cc-unique-url=${generateUuid()}`,
+                                            url : `https://www.bikeexchange.com/?cc-unique-url=${category.value}`,
                                             label : category.value,
                                             setData : {
                                                 category : category.label
@@ -163,116 +164,117 @@ async function awaitGlobal({condition}) {
         ],
         set : [
             {
-                callback : async () => {
-                    let productObjects = [],
-                        categorizedSets = await categorizedSetsOfflineDb.getAll();
+                callback : async (categorizedSet) => {
+                    let productObjects = [];
 
                     async function getProductsByCursor(label, endCursor = null)    {
                         
+                        await slowDown();
+
+                        console.clear();
+
                         let graphRequest = {
                                 url : "https://www.bikeexchange.com/graphql",
                                 method : "POST",
+                                headers : {
+                                    "Content-Type" : "application/json",
+                                },
                                 body : [
                                     {
                                         "operationName": "AdvertListQuery",
                                         "variables": {
-                                            "taxonSlug": `${label}`,
-                                            "cursor" : `${endCursor}`,
-                                            "brandSlugs": [
-                                                ""
-                                            ],
-                                            "regionSlug": "",
-                                            "locationSlug": "",
-                                            "postcode": "",
-                                            "keywordQuery": "",
-                                            "sellerIds": [
-                                                ""
-                                            ],
-                                            "sortBy": "DEFAULT",
-                                            "gender": [],
-                                            "condition": [],
-                                            "availability": [],
-                                            "modelSlug": "-",
-                                            "optionValueIds": [
-                                                ""
-                                            ],
+                                            "taxonSlug": label,
+                                            "cursor" : endCursor,
                                             "maxAdverts": 24,
-                                            "ratings": [
-                                                ""
-                                            ],
-                                            "searchDistance": 10,
-                                            "width": 400,
-                                            "height": 300,
-                                            "fit": "fillmax",
-                                            "logoHeight": 24
+                                            "fit": "fillmax"
                                         },
-                                        "query": "query AdvertListQuery($taxonSlug: String, $brandSlugs: [String!], $regionSlug: String, $locationSlug: String, $keywordQuery: String, $postcode: String, $sellerIds: [ID!], $optionValueIds: [ID!], $minPrice: Float, $maxPrice: Float, $sortBy: AdvertSearchSortByEnum, $cursor: String, $maxAdverts: Int, $width: Int, $height: Int, $fit: ImageFitEnum, $logoHeight: Int, $gender: [AdvertGenderEnum!], $condition: [AdvertConditionEnum!], $availability: [AdvertAvailabilityEnum!], $modelSlug: String, $ratings: [String!], $searchDistance: Int) {\n  advertSearch(\n    attributes: {taxonSlug: $taxonSlug, brandSlugs: $brandSlugs, regionSlug: $regionSlug, locationSlug: $locationSlug, keywordQuery: $keywordQuery, postcode: $postcode, sellerIds: $sellerIds, optionValueIds: $optionValueIds, sortBy: $sortBy, minPrice: $minPrice, maxPrice: $maxPrice, genders: $gender, conditions: $condition, availability: $availability, modelSlug: $modelSlug, ratings: $ratings, searchDistance: $searchDistance}\n  ) {\n    adverts: esAdverts(first: $maxAdverts, after: $cursor) {\n      edges {\n        node {\n          id\n          brand {\n            name\n            logo {\n              url: url(fit: crop, height: $logoHeight)\n              __typename\n            }\n            __typename\n          }\n          availability\n          cityDisplay\n          displayImage {\n            url: url(fit: $fit, width: $width, height: $height)\n            __typename\n          }\n          lowestPrice: lowestPriceInSessionCurrency\n          lowestOriginalPrice: lowestOriginalPriceInSessionCurrency\n          lowestFormattedPrice: salePriceFormatted\n          lowestOriginalFormattedPrice: originalPriceFormatted\n          isFeatured\n          isPromoted\n          pagePath\n          promotionText\n          promotionStartTime\n          promotionEndTime\n          seller {\n            businessName\n            shopAdvertSearchPath\n            isPrivate\n            managedMultiStore {\n              showParentInfo\n              __typename\n            }\n            __typename\n          }\n          taxon {\n            displayName\n            __typename\n          }\n          fullTitle\n          legacyId\n          rating {\n            count\n            stars\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n        __typename\n      }\n      totalCount\n      __typename\n    }\n    __typename\n  }\n  siteConfig {\n    id\n    advertBrandLogoVisible\n    hideAdvertTileSellerName\n    wishlistEnabled\n    advertSettings {\n      id\n      showLocationOnAdvertTile\n      permitsCartQuickView\n      __typename\n    }\n    yotpoSettings {\n      id\n      displayWidgetSearchPageEnabled\n      storeId\n      __typename\n    }\n    __typename\n  }\n}\n"
+                                        "query": "query AdvertListQuery(\n  $taxonSlug: String\n  $cursor: String\n  $maxAdverts: Int\n  $fit: ImageFitEnum\n) {\n  advertSearch(\n    attributes: {\n      taxonSlug: $taxonSlug\n    }\n  ) {\n    adverts: esAdverts(first: $maxAdverts, after: $cursor) {\n      edges {\n        node {\n          id\n          brand {\n            name\n          }\n          displayImage {\n            url: url(fit: $fit)\n          }\n          lowestPrice: lowestPriceInSessionCurrency\n          pagePath\n          fullTitle\n        }\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n      }\n      totalCount\n    }\n  }\n}"
                                     }
                                 ]
                             },
                             response = await fetch(graphRequest.url, {
                                 method : graphRequest.method,
-                                headers : {
-                                    "Content-Type" : "application/json",
-                                },
+                                headers : graphRequest.headers,
                                 body : JSON.stringify(graphRequest.body)
                             }),
-                            data = await response.json(),
-                            [graphResponse] = data,
+                            data = await response.json();
+
+                            console.log(data);
+                        let [graphResponse] = data,
                             scrapedProducts = getValidatedPropValues(graphResponse, ["data", "advertSearch", "adverts", "edges"]),
                             hasNextPage = getValidatedPropValues(graphResponse, ["data", "advertSearch", "adverts", "pageInfo", "hasNextPage"]),
+                            totalProducts = getValidatedPropValues(graphResponse, ["data", "advertSearch", "adverts", "totalCount"]),
                             newCursor = hasNextPage ? getValidatedPropValues(graphResponse, ["data", "advertSearch", "adverts", "pageInfo", "endCursor"]) : null;
                         
-                        console.log({graphResponse,scrapedProducts, newCursor});
+                        console.log({graphResponse, scrapedProducts, newCursor});
                         
-                        return {scrapedProducts, newCursor};    
+                        return {scrapedProducts, newCursor, totalProducts};    
 
                     }
+                    // await getProductsByCursor(categorizedSet.label, null);
 
                     async function recursiveApiCall(categorizedSet) {
 
                         let {label, setData} = categorizedSet,
-                            allProductObjects = [];
+                            allProductObjects = [],
+                            currentScrapedProducts = 0,
+                            page = 1,
+                            productsTotal = null;
 
                         async function getAllProductObjects(label, endCursor)   {
-                            let {scrapedProducts, newCursor} = await getProductsByCursor(label, endCursor);
 
-                            scrapedProducts = scrapedProducts.map(item => {
-                                let url = "https://www.bikeexchange.com"
-                                return {
-                                    productUri : `${url}${item.pagePath}`,
-                                    ...setData,
+                            try {
+                                let {scrapedProducts, newCursor, totalProducts} = await getProductsByCursor(label, endCursor);
+
+                                productsTotal = productsTotal ? productsTotal : totalProducts
+
+                                currentScrapedProducts = currentScrapedProducts + scrapedProducts.length;
+
+                                scrapedProducts = scrapedProducts.map(item => {
+                                    let url = "https://www.bikeexchange.com",
+                                        productUri = `${url}${getValidatedPropValues(item, ["node", "pagePath"])}`,
+                                        productName = getValidatedPropValues(item, ["node", "fullTitle"]),
+                                        brandName = getValidatedPropValues(item, ["node", "brand", "name"]),
+                                        imageUris = [getValidatedPropValues(item, ["node", "displayImage", "url"])].filter(item => item && item !== "");
+
+                                    return {
+                                        productUri,
+                                        productName,
+                                        brandName,
+                                        imageUris,
+                                        ...setData,
+                                    }
+                                });
+                                console.table(scrapedProducts);
+                                console.log({scrapedProducts, newCursor, currentScrapedProducts, productsTotal, page});
+                                allProductObjects.push(...scrapedProducts);
+                                page++;
+                                if(newCursor)   {
+                                    await getAllProductObjects(label, newCursor);
                                 }
-                            });
-                            console.log({scrapedProducts, newCursor});
-                            allProductObjects.push(...scrapedProducts);
-
-                            if(newCursor)   {
-                                await getAllProductObjects(label, newCursor);
+                            } catch(err)    {
+                                console.log(err);
                             }
+                            
                         }
                         
                         await getAllProductObjects(label, null);
 
-                        return allProductObjects;
+                        return {allProductObjects, currentScrapedProducts, productsTotal, page};
 
                     }
 
+                    let scrapingResult = await recursiveApiCall(categorizedSet),
+                        {allProductObjects, productsTotal, currentScrapedProducts, page} = scrapingResult;
 
-                    await moderator(categorizedSets.slice(0, 1), async (slicedArr) => {
+                    productObjects = allProductObjects;
 
-                        let promises = slicedArr.map(categorizedSet => {
-                            return async function() {
-                                let categorizedProducts = await recursiveApiCall(categorizedSet);
+                    console.table(productObjects);
 
-                                productObjects.push(...categorizedProducts);
-                            }
-                        });
+                    console.log({categorizedSet, productsTotal, currentScrapedProducts, page});
+                    await slowDown();
 
-                        await Promise.all(promises.map(item => item()));
-
-
-                    }, 1);    
+                    
 
                     return {
                         productObjects,
@@ -284,16 +286,52 @@ async function awaitGlobal({condition}) {
                 waitForSelectors : [
                     // ".product-item .product-image",
                 ],
+                args : [
+                    "categorizedSet",
+                ]
             },
         ],
         single : [
             {
                 callback : async () => {
 
+                    await slowDown(2525);
+
+                    let descriptionContainer = document.querySelector(".PageProduct-details .MRevealText .MAccordionContent-inner"),
+                        productDescription = descriptionContainer ? descriptionContainer.textContent.trim() : null,
+                        additionalProductDetailsContainer = document.querySelector(".PageProduct-details > div:first-child > p"),
+                        additionalProductDetails = function(){
+                            let productDetailsObject = {},
+                                excludedKeys = ["Brand", "Type", "Availability", "ID", "Item Condition", "Item condition", "item condition", "id", "Id"];
+                            if(additionalProductDetailsContainer) {
+                                let arr = additionalProductDetailsContainer.innerText.trim().split("\n").filter(item => item !== "").map(item => {
+                                    let [key, val] = item.split(":").map(item => item.trim());
+                                    return {key, val};
+                                });
+                                productDetailsObject = arr.reduce((a, b) => {
+                                    let {key, val} = b;
+                                    if(!excludedKeys.includes(key)) {
+                                        a[toCamelCase(key)] = val;
+                                    }
+                                    
+                                    return a;
+                                }, {});
+                            }
+
+                            return productDetailsObject;
+                        }();
+
+                    await slowDown(5252);
+
+                    return {
+                        productDescription,
+                        ...additionalProductDetails
+                    }
                 },
                 dataSource : "on-page",
                 waitForSelectors : [
                     // ".single-product-container .product-image"
+                    ".PageProduct-details"
                 ],
             }
         ]
@@ -313,19 +351,16 @@ async function awaitGlobal({condition}) {
             console.log(getValidatedPropValues(window, ["___cc__CcScraperGlobalObject", "evaluatorObject"]));
 
 
-            CcBrowserWindow.maxOpenedWindows = 5
-
-
             let categorizedSetsScraperObject = new CategorizedSetsScraper({
                 evaluatorObject : getValidatedPropValues(window, ["___cc__CcScraperGlobalObject", "evaluatorObject"]), 
                 executeMultiProductsSetsInitializer : true, 
-                executeMultiSingleProductInitializer : false, 
+                executeMultiSingleProductInitializer : true, 
                 addSetDataToProductProps : true,
                 uniqueProductObjProp : "productUri",
                 productUrlPropName : "productUri",
                 removeProductsWithoutUriPropName : true,
                 callbacksOnDone : [],
-                downloadZippedData : false,
+                downloadZippedData : true,
             });
 
             console.log(categorizedSetsScraperObject.categorizedSetsEvaluatorDone);
@@ -333,6 +368,28 @@ async function awaitGlobal({condition}) {
             await categorizedSetsScraperObject.getCategorizedSets();
             
             await categorizedSetsScraperObject.checkScraperDone();
+
+            /*
+
+                These code are used for testing purposes only.
+            
+            */
+            // let productsSet = await categorizedSetsOfflineDb.getOneById(`036cb48d-53bb-49dc-a66d-df749cea03ae`),
+            //     productObjects = await productsOfflineDb.getAllFilteredData({setId : productsSet.setId}),
+            //     multiSingleProductScraper = new MultiSingleProductInitializer({
+            //         setData : productsSet.setData, 
+            //         evaluatorObject : getValidatedPropValues(window, ["___cc__CcScraperGlobalObject", "evaluatorObject"]), 
+            //         setId : productsSet.setId,
+            //         productObjects,
+            //         uniqueProductObjProp : "productUri",
+            //         productUrlPropName : "productUri",
+            //         addSetDataToProductProps : true, 
+            //     });
+
+            // console.log(multiSingleProductScraper);
+            // await multiSingleProductScraper.getSingleProductData();
+
+            // await multiSingleProductScraper.checkScraperDone();
             
         }
     }
